34a27dcf4decf7a33444fb26220ef70714fdc463
Jordan Wheatley <jordanwheatley86@yahoo.com>
2015-05-24 23:46:46 -0400

    Ground Smash

--- src/me/cashvillan/doublejump/Listeners.java
+++ src/me/cashvillan/doublejump/Listeners.java
@@ -2,6 +2,7 @@ package me.cashvillan.doublejump;
 
 import org.bukkit.Effect;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.block.BlockFace;
@@ -13,12 +14,24 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 
 public class Listeners implements Listener {
 	
 	@EventHandler
 	public void onDamage(EntityDamageEvent event) {
 		Entity entity = event.getEntity();
+		Location loc = event.getEntity().getLocation();
+		
+		if (entity instanceof Player) {
+			if (event.getCause() == DamageCause.FALL) {
+				Player p = (Player) event.getEntity();
+				if (p.isSneaking()) {
+					entity.getWorld().playEffect(loc, Effect.EXPLOSION_HUGE, 152);
+					entity.getWorld().playSound(loc, Sound.ENDERDRAGON_HIT, 1, 2);
+				}
+			}
+		}
 		
 		if (entity instanceof Player) {
 			if (entity.isOp()) {
@@ -32,6 +45,7 @@ public class Listeners implements Listener {
 	@EventHandler
 	public void onMove(PlayerMoveEvent event) {
 		Player p = event.getPlayer();
+		
 		
 		if (p.getGameMode() != GameMode.CREATIVE) {
 			if (p.getLocation().getBlock().getRelative(BlockFace.DOWN).getType() != Material.AIR) {
@@ -49,6 +63,15 @@ public class Listeners implements Listener {
 			p.setVelocity(p.getLocation().getDirection().multiply(Float.parseFloat((String) FileManager.getValue("power"))).setY(Float.parseFloat((String) FileManager.getValue("height"))));
 			p.getWorld().playSound(p.getLocation(), Sound.ENDERDRAGON_WINGS, 1, 2);
 			p.getWorld().playEffect(p.getLocation(), Effect.STEP_SOUND, 64);
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onSneak(PlayerToggleSneakEvent event) {
+		Player p = event.getPlayer();
+		 if (p.isOnGround() == false) {
+				p.setVelocity(p.getLocation().getDirection().multiply(1).setY(-2));
 		}
 	}
 }
